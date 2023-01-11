178fc063bbdbac8abcc34174d82e5aba98b5c4c5
Matthew Johnson <matthew.dave.johnson@gmail.com>
2023-01-10 16:14:13 -0800

    Added ability to add new todo lists

--- todos-js175/todos.js
+++ todos-js175/todos.js
@@ -1,5 +1,7 @@
+/* eslint-disable max-lines-per-function */
 const express = require('express');
 const morgan = require('morgan');
+const TodoList = require('./lib/todolist');
 
 const app = express();
 const host = 'localhost';
@@ -12,12 +14,49 @@ app.set('view engine', 'pug');
 
 app.use(morgan('common'));
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+const compareByTitle = (todoListA, todoListB) => {
+  let titleA = todoListA.title.toLowerCase();
+  let titleB = todoListB.title.toLowerCase();
+
+  if (titleA < titleB) {
+    return -1;
+  } else if (titleA > titleB) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+const sortTodoLists = (lists) => {
+  let undone = lists.filter((todoList) => !todoList.isDone());
+  let done = lists.filter((todoList) => todoList.isDone());
+  undone.sort(compareByTitle);
+  done.sort(compareByTitle);
+  return [].concat(undone, done);
+};
 
 app.get('/', (req, res) => {
-  res.render('lists');
+  res.redirect('/lists');
 });
 
-// Listener
+app.get('/lists', (req, res) => {
+  res.render('lists', {
+    todoLists: sortTodoLists(todoLists),
+  });
+});
+
+app.get('/lists/new', (req, res) => {
+  res.render('new-list');
+});
+
+app.post('/lists', (req, res) => {
+  let title = req.body.todoListTitle.trim();
+  todoLists.push(new TodoList(title));
+  res.redirect('/lists');
+});
+
 app.listen(port, host, () => {
   console.log(`Todos is listening on port ${port} of ${host}!`);
 });
