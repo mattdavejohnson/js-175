007febe81d3138ff5a249ec8d6772e8ec0d3784a
Matthew Johnson <matthew.dave.johnson@gmail.com>
2023-01-11 00:27:06 -0800

    Added flash messaging

--- todos-js175/todos.js
+++ todos-js175/todos.js
@@ -1,6 +1,8 @@
 /* eslint-disable max-lines-per-function */
 const express = require('express');
 const morgan = require('morgan');
+const flash = require('express-flash');
+const session = require('express-session');
 const TodoList = require('./lib/todolist');
 
 const app = express();
@@ -15,6 +17,23 @@ app.set('view engine', 'pug');
 app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    name: 'launch-school-todos-session-id',
+    resave: false,
+    saveUninitialized: true,
+    secret: 'this is not very secure',
+  })
+);
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
 
 const compareByTitle = (todoListA, todoListB) => {
   let titleA = todoListA.title.toLowerCase();
@@ -53,22 +72,27 @@ app.get('/lists/new', (req, res) => {
 
 app.post('/lists', (req, res) => {
   let title = req.body.todoListTitle.trim();
+
   if (title.length === 0) {
+    req.flash('error', 'A title was not provided.');
     res.render('new-list', {
-      errorMessage: 'A title was not provided.',
+      flash: req.flash(),
     });
   } else if (title.length > 100) {
+    req.flash('error', 'List title must be between 1 and 100 characters.');
     res.render('new-list', {
-      errorMessage: 'List title must be between 1 and 100 characters.',
-      todoListTitle: title,
+      flash: req.flash(),
+      todoListTitle: req.body.todoListTitle,
     });
   } else if (todoLists.some((list) => list.title === title)) {
+    req.flash('error', 'List title must be unique.');
     res.render('new-list', {
-      errorMessage: 'List title must be unique.',
-      todoListTitle: title,
+      flash: req.flash(),
+      todoListTitle: req.body.todoListTitle,
     });
   } else {
     todoLists.push(new TodoList(title));
+    req.flash('success', 'The todo list has been created.');
     res.redirect('/lists');
   }
 });
